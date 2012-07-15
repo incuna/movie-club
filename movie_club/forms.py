8703823684a8aa911553b6cfeb2a137ea571c6fa
George Hickman <george@ghickman.co.uk>
2012-07-15 13:48:47 +0100

    set the available scores

--- movie_club/forms.py
+++ movie_club/forms.py
@@ -7,6 +7,7 @@ from .models import Rating
 class RatingForm(forms.ModelForm):
     user = forms.ChoiceField(
         choices=User.objects.exclude(username='admin').values_list('pk', 'first_name'))
+    score = forms.ChoiceField(choices=zip(range(1,11), range(1,11)))
 
     class Meta:
         fields = ('user', 'score')
