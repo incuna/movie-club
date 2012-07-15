97bc39d4bb5048172e316cd04af2f73e2e92edd4
George Hickman <george@ghickman.co.uk>
2012-07-15 13:18:19 +0100

    Remove admin from the user list for rating

--- movie_club/forms.py
+++ movie_club/forms.py
@@ -1,9 +1,13 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from .models import Rating
 
 
 class RatingForm(forms.ModelForm):
+    user = forms.ChoiceField(
+        choices=User.objects.exclude(username='admin').values_list('pk', 'first_name'))
+
     class Meta:
         fields = ('user', 'score')
         model = Rating
